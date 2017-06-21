937d203566129a70ad321ead855df1232a1e6b82
Igor Rafael <yggor14rafa@hotmail.com>
2017-06-21 00:12:39 -0300

    Updating files

--- CNA.py
+++ CNA.py
@@ -49,13 +49,57 @@ class CNA():
                                 mat[i][j] = sheet.cell_value(i+2, j+2)
                 return mat
 
+        def mat2_xls(self, mat2):
+                """Writes the resulting matrix in a xls file
+                """
+                rb = open_workbook(self.xls_final_name, encoding_override="cp1252", formatting_info=True)
+                wb = copy(rb)
+                sheet = wb.get_sheet(1)
+                style = easyxf('font: name Arial, color black; align: wrap on, vert centre, horiz center; borders: left thin, right thin, top thin, bottom thin;')
+                machines, products = self.get_matrix_info()
+                sheet.write(1, 1, "Indice", style)
+                for i in range(len(machines)): #Writing the machine's names on the workbook
+                        sheet.write(i + 2, 0, machines[i])
+                for i in range(len(machines)): #Writing the machine's names on the workbook
+                        sheet.write(i + 2, 1, i + 1)                
+                for j in range(len(machines)): #Writing the machine's names on the workbook
+                        sheet.write(0, j + 2, machines[j])
+                for j in range(len(machines)): #Writing the machine's names on the workbook
+                        sheet.write(1, j + 2, j + 1)                
+                for i in range(len(mat2)):
+                        for j in range(len(mat2[i])):
+                                sheet.write(i + 2, j + 2, mat2[i][j], style)
+                wb.save(self.xls_final_name)
+
+        def mat3_xls(self, mat3, ordemlinhas):
+                """Writes the resulting matrix in a xls file
+                """
+                rb = open_workbook(self.xls_final_name, encoding_override="cp1252", formatting_info=True)
+                wb = copy(rb)
+                sheet = wb.get_sheet(2)
+                style = easyxf('font: name Arial, color black; align: wrap on, vert centre, horiz centre; borders: left thin, right thin, top thin, bottom thin;')
+                machines, products = self.get_matrix_info()
+                sheet.write(1, 1, "Indice", style)
+                for j in range(self.num_cols): #Writing the product's names on the workbook
+                        sheet.write(0, j + 2, products[j])
+                for j in range(self.num_cols): #Writing the product's names on the workbook
+                        sheet.write(1, j + 2, j + 1)
+                for i in range(len(ordemlinhas)): #Writing the machine's names on the workbook
+                        sheet.write(i + 2, 0, machines[ordemlinhas[i]])
+                for i in range(len(ordemlinhas)): #Writing the machine's names on the workbook
+                        sheet.write(i + 2, 1, ordemlinhas[i] + 1)
+                for i in range(len(mat3)):
+                        for j in range(len(mat3[i])):
+                                sheet.write(i + 2, j + 2, mat3[i][j], style)
+                wb.save(self.xls_final_name)
+
         def mat4_xls(self, mat4, index_mach, index_prod):
                 """Writes the resulting matrix in a xls file
                 """
                 rb = open_workbook(self.xls_final_name, encoding_override="cp1252", formatting_info=True)
                 wb = copy(rb)
-                sheet = wb.get_sheet(0)
-                style = easyxf('font: name calibri, color black; align: wrap on, vert centre, horiz center; borders: left thin, right thin, top thin, bottom thin;')
+                sheet = wb.get_sheet(3)
+                style = easyxf('font: name Arial, color black; align: wrap on, vert centre, horiz center; borders: left thin, right thin, top thin, bottom thin;')
                 machines, products = self.get_matrix_info()
                 for j in range(len(mat4[0])): #Writing the product's names on the workbook
                         sheet.write(0, j + 2, products[index_prod[j]])
@@ -82,21 +126,19 @@ class CNA():
                                                         tasks_per_machine[m] += 1
                                                         mat2[i][m] += 1
                                                         mat2[m][i] += 1
+ #               self.mat2_xls(mat2)
                 return [mat2, tasks_per_machine]
 
         def mat3(self, matriz1, matriz2, tasks_per_machine):
                 """Calculates the matrix 3, based on the matrix 2.
                 """
                 ordemlinhas = []
-
                 #Primeira Linha
-
                 maior = 0
                 for i in xrange(1,len(tasks_per_machine)):
                         if tasks_per_machine[i] > tasks_per_machine[maior]:
                                 maior = i
                 ordemlinhas.append(maior)
-
                 #Restante das Linhas
                 while(len(ordemlinhas)<len(tasks_per_machine)):
                         i = ordemlinhas[-1]
@@ -112,12 +154,10 @@ class CNA():
                                                 if tasks_per_machine[j] > tasks_per_machine[maior]:
                                                         maior = j
                         ordemlinhas.append(maior)
-
                 #Construcao da nova matriz com as linhas reordenadas2
                 novaMat = []
                 for i in ordemlinhas:
                         novaMat.append(matriz1[i])
-
                 return [novaMat, ordemlinhas]
 
         def contagem(self, seq, coluna):
@@ -135,15 +175,12 @@ class CNA():
                 mat = matriz3[:]
                 ordemColunas = []
                 colunas = len(mat[0])
-                
                 while True:
-
                         #Se a matriz ainda esta divisivel
                         if len(mat) > 1:
                                 div = len(mat)/2
                                 seq1 = mat[:div]
                                 seq2 = mat [div:]
-
                                 #Adiciona a coluna na nova ordem se o numero de 1s
                                 #na coluna for maior em SEQ1 que em SEQ2
                                 for j in xrange(colunas):
@@ -152,14 +189,12 @@ class CNA():
                                                 n2 = self.contagem(seq2,j)
                                                 if n1 > n2:
                                                         ordemColunas.append(j)
-
                                 #Se todas as linhas ja foram marcadas o processo para,
                                 #caso contrario a nova matriz sera SEQ2
                                 if len(ordemColunas) == colunas:
                                         break
                                 else:
                                         mat = seq2
-                                        
                         #Caso nao seja mais possivel dividir a matriz
                         #adicionam-se as linhas restantes diretamente
                         else:
@@ -167,7 +202,6 @@ class CNA():
                                         if j not in ordemColunas:
                                                 ordemColunas.append(j)
                                 break
-
                 #Construcao da nova matriz, com linhas e colunas rearranjadas
                 novaMat = []
                 for i in matriz3:
@@ -175,13 +209,12 @@ class CNA():
                         for j in ordemColunas:
                                 linha.append(i[j])
                         novaMat.append(linha)
-
                 return [novaMat, ordemColunas]
 
 if __name__ == "__main__":
         filename = "matriz1.txt"
         filename_xls = "Analise de Fluxo Produtos e Máquinas Final.xls"
-        final_xls = "Matriz Final.xls"
+        final_xls = "Matriz.xls"
         cna = CNA(filename, filename_xls, final_xls, 27, 30)
         matriz1 = cna.mat1_xls()
         print 'Matriz A'
